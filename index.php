2d3b9d3506212736ef663e4f7036f2f2908e6e44
Tamara199895 <56243171+Tamara199895@users.noreply.github.com>
2020-05-23 23:00:08 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Graph</title>
+</head>
+<body>
+ <canvas id="myCanvas" width="650px" height="550px"></canvas>
+<?php
+ include 'graph.php';
+// include 'digraph.php'; ?>
+</body>
+</html>
